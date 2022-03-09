2e68ab16383d03b052f00fc3db156084b4a0c761
adrjob <98612841+adrjob@users.noreply.github.com>
2022-03-09 13:42:43 +0300

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,26 +14,24 @@ class UsersTableSeeder extends Seeder
     {
         factory(App\User::class)->create([
             'id' => 1,
-            'name' => 'Admin',
-            'email' => 'admin@white.com',
+            'name' => 'Saleh',
+            'email' => 'saleh@salehabdul.com',
             'role_id' => 1,
-            'picture' => '../white/img/jana.jpg'
         ]);
 
-        factory(App\User::class)->create([
-            'id' => 2,
-            'name' => 'Creator',
-            'email' => 'creator@white.com',
-            'role_id' => 2,
-            'picture' => '../white/img/lora.jpg'
-        ]);
+//         factory(App\User::class)->create([
+//             'id' => 2,
+//             'name' => 'Creator',
+//             'email' => 'creator@white.com',
+//             'role_id' => 2,
+//             'picture' => '../white/img/lora.jpg'
+//         ]);
 
         factory(App\User::class)->create([
             'id' => 3,
-            'name' => 'Member',
-            'email' => 'member@white.com',
+            'name' => 'Adrian',
+            'email' => 'adrian@salehabdul.com',
             'role_id' => 3,
-            'picture' => '../white/img/mike.jpg'
         ]);
     }
 }
