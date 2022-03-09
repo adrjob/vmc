7815c0f1792fadc1a95cab83a3f5154afdad3c3b
adrjob <adrian@salehabdul.com>
2022-03-09 13:37:53 +0300

    init commit

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Member;
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $members = Member::all();
+
+        return view('members.index', compact('members'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Member;
+
+        $data->name = $request->name;
+        $data->user_id = auth()->user()->id;
+
+        if($data->save())
+        {
+            return redirect()->route('member.edit', $data->id);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Member $member)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Member $member)
+    {
+        return view('members.edit', compact('member'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Member $member)
+    {
+
+        $data = Member::find($member->id);
+
+        $data->name = $request->name;
+        $data->gender = $request->gender;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+
+        if($data->save())
+        {
+            return redirect()->back()->withStatus('Member has been updated');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Member $member)
+    {
+        $member->delete();
+
+        return redirect()->back()->withStatus('Member has been deleted');
+    }
+}
---
+++ resources/views/members/edit.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app', ['activePage' => 'member-management', 'menuParent' => 'member', 'titlePage' => __('Member Management')])
+
+@section('content')
+    <style>
+        input {
+            width: 100%;
+        }
+    </style>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <form method="post" enctype="multipart/form-data" action="{{ route('member.update', $member) }}" autocomplete="off">
+            @csrf
+            @method('put')
+            <div class="card ">
+              <div class="card-header">
+                <h4 class="card-title">{{ __('Edit Member') }}</h4>
+              </div>
+              <div class="card-body ">
+                <div class="row">
+                  <div class="col-md-12 text-right">
+                      <a href="{{ route('member.index') }}" class="btn btn-sm btn-primary">{{ __('Back to list') }}</a>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Profile photo') }}</label>
+                  <div class="col-sm-7">
+                    <div class="fileinput fileinput-new text-center" data-provides="fileinput">
+{{--                      <div class="fileinput-new thumbnail img-circle">--}}
+{{--                        @if ($user->picture)--}}
+{{--                          <img src="{{ $user->profilePicture() }}" alt="...">--}}
+{{--                        @else--}}
+{{--                          <img src="{{ asset('white') }}/img/placeholder.jpg" alt="...">--}}
+{{--                        @endif--}}
+{{--                      </div>--}}
+                      <div class="fileinput-preview fileinput-exists thumbnail img-circle"></div>
+                      <div>
+                        <span class="btn btn-file btn-sm btn-primary">
+                          <span class="fileinput-new">{{ __('Select image') }}</span>
+                          <span class="fileinput-exists">{{ __('Change') }}</span>
+                          <input type="file" name="photo" id = "input-picture" />
+                        </span>
+                          <a href="#pablo" class="btn btn-danger fileinput-exists" data-dismiss="fileinput"><i class="fa fa-times"></i> {{ __('Remove') }}</a>
+                      </div>
+                      @include('alerts.feedback', ['field' => 'photo'])
+                    </div>
+                  </div>
+                </div>
+                <!-- Row 1 -->
+                <div class="row">
+                  <div class="col">
+                      <label class="col-form-label">{{ __('Name') }}</label>
+                      <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                          <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="input-name" type="text" placeholder="{{ __('Name') }}" value="{{ old('name', $member->name) }}" required="true" aria-required="true"/>
+                          @include('alerts.feedback', ['field' => 'name'])
+                      </div>
+                  </div>
+                  <div class="col">
+                      <label class="col-form-label">{{ __('Gender') }}</label>
+                      <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                          <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="gender" id="input-email" type="text" placeholder="{{ __('Gender') }}" value="{{ old('gender', $member->gender) }}" />
+                          @include('alerts.feedback', ['field' => 'email'])
+                      </div>
+                  </div>
+                </div>
+                <!-- Row 2 -->
+                <div class="row">
+                  <div class="col">
+                      <label class="col-form-label">{{ __('Email') }}</label>
+                      <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                          <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="email" id="input-name" type="email" placeholder="{{ __('Email') }}" value="{{ old('email', $member->email) }}"/>
+                          @include('alerts.feedback', ['field' => 'name'])
+                      </div>
+                  </div>
+                  <div class="col">
+                      <label class="col-form-label">{{ __('Mobile Phone') }}</label>
+                      <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                          <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="phone" id="input-email" type="text" placeholder="{{ __('Phone') }}" value="{{ old('phone', $member->phone) }}"/>
+                          @include('alerts.feedback', ['field' => 'email'])
+                      </div>
+                  </div>
+                </div>
+
+
+              </div>
+              <div class="card-footer ml-auto mr-auto">
+                <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
